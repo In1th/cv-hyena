3ce9398a5b945ca5d66c0d23e4252ed5143c7ee0
Mateusz Kruk <krukm634@gmail.com>
2023-10-03 19:50:03 +0200

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,svelte}"],
+  theme: {
+    extend: {},
+    colors: {
+      'text': '#f7f5f2',
+      'background': '#16120e',
+      'primary': '#F77410',
+      'secondary': '#1c1711',
+      'accent': '#574e48',
+     },
+  },
+  plugins: [],
+}
+
